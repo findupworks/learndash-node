4723fba2ec6266da4ea5440b879c0278bb674647
AndreAntunesBarros <32828459+AndreAntunesBarros@users.noreply.github.com>
2022-09-15 15:11:10 -0300

    fixes

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,18 @@
 import Users from "./modules/Users";
 import Courses from "./modules/Courses";
+import Categories from "./modules/Categories";
 import dbInit from '../db/init';
 class LearnDash {
     public users: Users;
     public courses: Courses;
+    public categories: Categories;
 
     constructor() {
         dbInit()
 
         this.users = new Users();
         this.courses = new Courses();
+        this.categories = new Categories();
     }
 }
 
